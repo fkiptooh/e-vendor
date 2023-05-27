552ae15c8e46d494c788ab2c9b21d6fbd4148bd4
fkiptooh <fkiptooh@gmail.com>
2023-05-27 17:10:59 +0300

    Mongo atlas connection to our server

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const app = require("./app");
+const dbConnection = require("./db/Database");
+
+// Handling uncaught exceptions
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server for handling the uncaught exceptions");
+});
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "./config/.env",
+  });
+}
+
+// dbconnection
+dbConnection()
+
+// creating our server
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+// unhandle promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`Shutting down the server for unhandle promise rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
