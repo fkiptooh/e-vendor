6120e68f08f7ad3cf52f25c7353d7703b1590b47
fkiptooh <fkiptooh@gmail.com>
2023-06-10 15:59:45 +0300

    Popular events timer

--- frontend/src/components/Route/Events/CountDown.jsx
+++ frontend/src/components/Route/Events/CountDown.jsx
@@ -7,23 +7,45 @@ const CountDown = () => {
       setTimeLeft(calculateTimeLeft());
     }, 1000);
     return () => clearTimeout(timer);
-  }, []);
+  });
 
-  function calculateTimeLeft(){
-    const difference  = + new Date('2023-6-9') - +new Date();
-    let timeLeft = {}
+  function calculateTimeLeft() {
+    const difference = +new Date("2023-6-12") - +new Date();
+    let timeLeft = {};
 
-    if(difference > 0){
-        timeLeft = {
-            days: Math.floor(difference /(1000 * 60 *60 *24)),
-            hours: Math.floor((difference /(1000 * 60 *60)) % 24),
-            minutes: Math.floor((difference / 1000 * 60)% 60 ),
-            seconds: Math.floor((difference / 1000) % 60),
-
-        };
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
     }
+
+    return timeLeft;
   }
-  return <div>CountDown</div>;
+
+  const timerComponents = Object.keys(timeLeft).map((interval) => {
+    if (!timeLeft[interval]) {
+      return null;
+    }
+
+    return (
+      <span className="text-[25px] text-[#475ad2]">
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  return (
+    <div>
+      {timerComponents.length ? (
+        timerComponents
+      ) : (
+        <span className="text-[red] text-[25px]">Time's Up</span>
+      )}
+    </div>
+  );
 };
 
 export default CountDown;
