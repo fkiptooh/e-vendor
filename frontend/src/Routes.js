900063ef68a62dd22f1234721132bdebe740627b
fkiptooh <fkiptooh@gmail.com>
2023-06-13 00:51:25 +0300

    Filter on products by categoty

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,11 +1,13 @@
 import Login from "../src/pages/Login";
 import SignUp from "./pages/SignUp";
 import Activation from "./pages/Activation";
-import HomePage from "../src/pages/HomePage.jsx";
+import HomePage from "../src/pages/HomePage";
+import ProductsPage from "../src/pages/ProductsPage"
 
 export {
     Login,
     SignUp,
     Activation,
-    HomePage
+    HomePage,
+    ProductsPage
 }
